c94854010ac113afc864df1cfbd778a8720276e5
Fortanono <henryrosser74@gmail.com>
2016-07-26 06:32:54 -0400

    Added basic things (not currently working)

---
+++ prototypes/item.lua
@@ -0,0 +1,12 @@
+data.extend({
+
+  {
+    type= "item",
+    name= "algae",
+    icon= "__Algaculture__/graphics/icons/algae.png",
+    flags= { "goes-to-main-inventory" },
+    subgroup= "raw-materials",
+    stack_size= 100,
+  }
+  
+})
---
+++ prototypes/recipe.lua
@@ -0,0 +1,19 @@
+data.extend({
+
+  {
+    type = "recipe",
+    name = "algae",
+    category = "chemistry",
+    enabled = true,
+    energy_required = 1,
+    ingredients = 
+    {
+      {type="fluid", name="water", amount=5},
+      {type="fluid", name="fertilizer", amount=3}
+    },
+    result = 
+    {
+      {type="item", name="algae", amount=2}
+    }
+  }
+})
